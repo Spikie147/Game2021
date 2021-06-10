6fa0ce1354c7465c89516805d7a4907d95bf8f19
Spikie147 <codbro75@kingshigh.school.nz>
2021-06-11 10:58:50 +1200

    Assassination Abraham 2
    sskksssks

--- Asssassination/Optionsmenu.gd
+++ Asssassination/Optionsmenu.gd
@@ -1,42 +1,39 @@
 extends ColorRect
 
 
+onready var mode_select = $CenterContainer/VBoxContainer/CenterContainer/HBoxContainer/Difficulty
 
-onready var easy_mode = $CenterContainer/VBoxContainer/CenterContainer2/HBoxContainer/Difficulty
+onready var selector_one = $CenterContainer/VBoxContainer/CenterContainer/HBoxContainer/Selector
+onready var selector_two = $CenterContainer/VBoxContainer/CenterContainer2/HBoxContainer2/SelectorTwo
 
-onready var selector_one = $CenterContainer/VBoxContainer/CenterContainer2/HBoxContainer/Selector
-onready var selector_two = $CenterContainer/VBoxContainer/CenterContainer/HBoxContainer2/Selector2
+var current_selection = 0
+var difficulty = 0
 
 
-var current_selection = 2
-var currentselection = 0 
-
-func ready():
+func _ready():
 	set_current_selection(0)
-	set_currentselection(0)
 
-func _process(delta):
+
+func _process(_delta):
 	if Input.is_action_just_pressed("ui_down") and current_selection < 1:
 		current_selection += 1
 		set_current_selection(current_selection)
 	elif Input.is_action_just_pressed("ui_up") and current_selection > 0:
-		currentselection -= 1
+		current_selection -= 1
 		set_current_selection(current_selection)
-	elif Input.is_action_just_pressed("ui_accept"):
-		set_current_selection(current_selection)
+	elif Input.is_action_just_pressed("ui_accept") and current_selection >=1:
+		OS.window_fullscreen = !OS.window_fullscreen
 
 
-func set_currentselection(_currentselection):
-	easy_mode.text = "Easy"
-	if currentselection == 1:
-		easy_mode.text = "Normal"
+func _on_Window_Size_pressed():
+	if Input.is_action_just_pressed("ui_accept") and current_selection >=1:
+		OS.window_fullscreen = !OS.window_fullscreen
+
 
 func set_current_selection(_current_selection):
 	selector_one.text = ""
 	selector_two.text = ""
-	if current_selection == 1:
+	if current_selection == 0:
 		selector_one.text = ">"
-	elif current_selection == 2:
+	elif current_selection == 1:
 		selector_two.text = ">"
-
-
