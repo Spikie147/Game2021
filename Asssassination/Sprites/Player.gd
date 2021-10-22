07b04fa85340f2eed13c7cea0621c3e70e035ab9
Spikie147 <codbro75@kingshigh.school.nz>
2021-10-22 14:26:33 +1300

    Friday

--- Asssassination/Sprites/Player.gd
+++ Asssassination/Sprites/Player.gd
@@ -1,10 +1,12 @@
 extends KinematicBody2D
 
-export (int) var speed = 80.0
-export (int) var jump_speed = -200
+export (int) var speed = 160.0
+export (int) var jump_speed = -225
 export (int) var gravity = 900
 export (float, 0, 1.0) var friction = 0.2
 export (float, 0, 1.0) var acceleration = 0.25
+
+# these are the variables that are used thru
 
 var velocity = Vector2.ZERO
 
@@ -15,26 +17,27 @@ func _physics_process(delta):
 	if Input.is_action_just_pressed("ui_up"):
 		if is_on_floor():
 			velocity.y = jump_speed
-
+# this code here gives the gravity so when I press the spacebar I dont continuously rise, this will make me fall at an accelerated speed
 
 
 func get_input():
 	var dir = 0
 	if Input.is_action_pressed("ui_right"):
-		dir += 1
+		dir = 1
 		$AnimationPlayer.play("Running")
-		$AnimationPlayer.flip_z = true
+		$Sprite.flip_h= true
 	if Input.is_action_pressed("ui_left"):
-		dir -= 1
+		dir = -1
 		$AnimationPlayer.play("Running")
-		$AnimationPlayer.flip_z = false
+		$Sprite.flip_h= false
 	if dir != 0:
-		velocity.x = lerp(velocity.x, 0, friction)
+		velocity.x = lerp(velocity.x, speed*dir, acceleration)
 		$AnimationPlayer.play("Running")
 	else:
 		velocity.x = lerp(velocity.x, 0, friction)
 		$AnimationPlayer.play("Idle")
 # Written the code myself, does not work as intended and have been trying to fix without help. This did not work very well. I based my code off of 
 # previous attemps and some videos on youtube. 
+# this code is the animation of the player when he moves/does not move.
 
 
