8b6e5c971bea6669e5ca148f0dac12f188c91d94
Spikie147 <codbro75@kingshigh.school.nz>
2021-11-10 14:59:17 +1300

    fbery

--- Asssassination/Sprites/Player.gd
+++ Asssassination/Sprites/Player.gd
@@ -19,23 +19,27 @@ func _physics_process(delta):
 			velocity.y = jump_speed
 # this code here gives the gravity so when I press the spacebar I dont continuously rise, this will make me fall at an accelerated speed
 
-
+ 
 func get_input():
 	var dir = 0
 	if Input.is_action_pressed("ui_right"):
-		dir = 1
-		$AnimationPlayer.play("Running")
-		$Sprite.flip_h= true
+		if is_on_floor():
+			dir = 1
+			$AnimationPlayer.play("Running")
+			$Sprite.flip_h= true
 	if Input.is_action_pressed("ui_left"):
-		dir = -1
-		$AnimationPlayer.play("Running")
-		$Sprite.flip_h= false
+		if is_on_floor():
+			dir = -1
+			$AnimationPlayer.play("Running")
+			$Sprite.flip_h= false
 	if dir != 0:
-		velocity.x = lerp(velocity.x, speed*dir, acceleration)
-		$AnimationPlayer.play("Running")
+		if is_on_floor():
+			velocity.x = lerp(velocity.x, speed*dir, acceleration)
+			$AnimationPlayer.play("Running")
 	else:
-		velocity.x = lerp(velocity.x, 0, friction)
-		$AnimationPlayer.play("Idle")
+		if is_on_floor():
+			velocity.x = lerp(velocity.x, 0, friction)
+			$AnimationPlayer.play("Idle")
 # Written the code myself, does not work as intended and have been trying to fix without help. This did not work very well. I based my code off of 
 # previous attemps and some videos on youtube. 
 # this code is the animation of the player when he moves/does not move.
